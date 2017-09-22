5e6289a88826a5ad86673291914f5426f2d13991
Marco Cattaneo <marcocatta@gmail.com>
2017-09-22 14:50:08 +0200

    fail edit

--- app/src/main/java/eu/marcocattaneo/roomdagger2/repository/DemoDatabase.java
+++ app/src/main/java/eu/marcocattaneo/roomdagger2/repository/DemoDatabase.java
@@ -1,3 +1,11 @@
+package eu.marcocattaneo.roomdagger2.repository;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+import eu.marcocattaneo.roomdagger2.data.Product;
+import eu.marcocattaneo.roomdagger2.data.ProductDao;
+
 @Database(entities = {Product.class}, version = DemoDatabase.VERSION)
 public abstract class DemoDatabase extends RoomDatabase {
 
